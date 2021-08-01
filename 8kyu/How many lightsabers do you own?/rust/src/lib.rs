1dc768a3566d4906a2b7536df3c96277d7ac7524
Custer <custertian@163.com>
2021-08-01 13:24:13 +0800

    How many lightsabers do you own?

---
+++ 8kyu/How many lightsabers do you own?/rust/src/lib.rs
@@ -0,0 +1,38 @@
+fn how_many_lightsabers_do_you_own(name: &str) -> u8 {
+    if name == "Zach" {
+        return 18_u8;
+    }
+    return 0_u8;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn returns_zero_for_an_empty_string() {
+        assert_eq!(how_many_lightsabers_do_you_own(""), 0);
+    }
+    #[test]
+    fn returns_0_for_other_people() {
+        assert_eq!(how_many_lightsabers_do_you_own("Adam"), 0);
+    }
+    #[test]
+    fn returns_18_for_Zach() {
+        assert_eq!(how_many_lightsabers_do_you_own("Zach"), 18);
+    }
+    #[test]
+    fn returns_0_when_Zach_is_lowercased() {
+        assert_eq!(how_many_lightsabers_do_you_own("zach"), 0);
+    }
+
+    fn best_solv(name: &str) -> u8 {
+        match name {
+            "Zach" => 18,
+            _ => 0,
+          }
+    }
+    fn optimization(name: &str) -> u8 {
+        if name == "Zach" { 18 } else { 0 }
+      }
+}
